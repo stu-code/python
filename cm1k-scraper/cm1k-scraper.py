234ab3ae014be4cfb18641fc290def8e68aa4bb6
Stu Sztukowski <Stu.Sztukowski@sas.com>
2022-09-08 10:38:57 -0400

    Update folder

--- cm1k-scraper/cm1k-scraper.py
+++ cm1k-scraper/cm1k-scraper.py
@@ -32,7 +32,7 @@ from time import sleep
 url       = 'http://192.168.100.1/DocsisStatus.asp'
 user      = 'admin'
 pw        = 'admin'
-outfolder = "C:\\Users\\Stu\\Desktop" # Do not include ending \\
+outfolder = "B:\\Data\\cm1k-scraper" # Do not include ending \\
 
 ###################################################
 #################### Begin ########################
